b9197297db5111b2afb7cca04ae44106639be148
Арсений Ш <cenz1217@gmail.com>
2024-03-25 21:49:54 +0500

    fixed prev commit

--- UTMNStudentsExamAnalysis/ClientApp/src/app/report-generator/report-generator.component.ts
+++ UTMNStudentsExamAnalysis/ClientApp/src/app/report-generator/report-generator.component.ts
@@ -9,8 +9,10 @@ export class ReportGeneratorComponent implements OnInit {
   public filterOptions = ["Таблица", "Гистограмма",
     "Круговая диаграмма", "Диаграмма рассеяния"];
 
-  public schoolOptions = ["63", "83"];
+  public schoolOptions = ["63", "83", "63", "83"];
   public classOptions = ["11A", "11B"];
+  public typeOptions = ["ЕГЭ", "ОГЭ"];
+  public subjectOptions = ["Математика", "Русский"];
   constructor() { }
 
   ngOnInit(): void {
