5f4c69778e11f86e5b11cbbdf5ffd9ca67b1d60f
Arseniy <cenz1217@gmail.com>
2024-05-06 04:37:49 +0500

    added new admin features

--- UTMNStudentsExamAnalysis/ClientApp/src/app/admin/admin.component.ts
+++ UTMNStudentsExamAnalysis/ClientApp/src/app/admin/admin.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { NgxFileDropEntry } from 'ngx-file-drop';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +8,69 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+  selectedIndex: number = 0;
+  accept: string = ".xlsx, .xls, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" 
 
-  constructor() { }
+  fileControl: FormControl;
+
+  public files: any;
+
+  constructor() {
+    this.fileControl = new FormControl(this.files)
+  }
 
   ngOnInit(): void {
+    this.fileControl.valueChanges.subscribe((files: any) => {
+      if (!Array.isArray(files)) {
+        this.files = [files];
+      } else {
+        this.files = files;
+      }
+    })
+  }
+  public dropped(files: NgxFileDropEntry[]) {
+    this.files = files;
+    for (const droppedFile of files) {
+
+      // Is it a file?
+      if (droppedFile.fileEntry.isFile) {
+        const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
+        fileEntry.file((file: File) => {
+
+          // Here you can access the real file
+          console.log(droppedFile.relativePath, file);
+
+          /**
+          // You could upload it like this:
+          const formData = new FormData()
+          formData.append('logo', file, relativePath)
+
+          // Headers
+          const headers = new HttpHeaders({
+            'security-token': 'mytoken'
+          })
+
+          this.http.post('https://mybackend.com/api/upload/sanitize-and-save-logo', formData, { headers: headers, responseType: 'blob' })
+          .subscribe(data => {
+            // Sanitized logo returned from backend
+          })
+          **/
+
+        });
+      } else {
+        // It was a directory (empty directories are added, otherwise only files)
+        const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
+        console.log(droppedFile.relativePath, fileEntry);
+      }
+    }
+  }
+
+  public fileOver(event: any) {
+    console.log(event);
+  }
+
+  public fileLeave(event: any) {
+    console.log(event);
   }
 
 }
--- UTMNStudentsExamAnalysis/ClientApp/src/app/app.module.ts
+++ UTMNStudentsExamAnalysis/ClientApp/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { MatTabsModule } from '@angular/material/tabs';
+import { NgxMatFileInputModule } from '@angular-material-components/file-input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -21,6 +28,8 @@ import { UserDropdownComponent } from './user-dropdown/user-dropdown.component';
 import { JwtModule } from '@auth0/angular-jwt';
 import { PasswordRestoreComponent } from './password-restore/password-restore.component';
 import { AdminComponent } from './admin/admin.component';
+import { MatButtonModule } from '@angular/material/button';
+import { NgxFileDropModule } from 'ngx-file-drop';
 
 @NgModule({
   declarations: [
@@ -34,7 +43,7 @@ import { AdminComponent } from './admin/admin.component';
     LoginComponent,
     RegisterComponent,
     PasswordRestoreComponent,
-    AdminComponent
+    AdminComponent,
     RegisterComponent,
     UserDropdownComponent
   ],
@@ -44,6 +53,19 @@ import { AdminComponent } from './admin/admin.component';
     FormsModule,
     BrowserAnimationsModule,
     MatTabsModule,
+    MatInputModule,
+    NgxMatFileInputModule,
+    NgxFileDropModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatCardModule,
+    NgxMatFileInputModule,
     ToastrModule.forRoot(),
     JwtModule.forRoot({
       config: {
